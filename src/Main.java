aaebca315764a21840c68ed7487c9e3b94b6abfc
martyna.radz <mar.rad97@gmail.com>
2024-05-04 09:00:33 +0200

    zad3

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@ public class Main {
     public static void main(String[] args) {
         try {
             company.beans.Person person = new company.beans.Person("Jan Nowak", 30);
-            System.out.println("Imie: " + person.getName() + ", Wiek: " + person.getAge());
+            System.out.println("Imie, nazwisko: " + person.getName() + ", Wiek: " + person.getAge());
         } catch (company.exceptions.InvalidAgeException e) {
             System.err.println(e.getMessage());
         }
+        int sum = company.utils.MathUtils.add(10, 20);
+        System.out.println("10+20=" + sum);
         company.interfaces.Messenger messenger = new company.implementations.EmailMessenger();
-        messenger.sendMessage("Dzień dobry!");
+        messenger.sendMessage("Wynik dodawania to: "+sum);
     }
 }
