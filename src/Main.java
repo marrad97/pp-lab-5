0e2094ca9d72cf7708900bda1bc2e272a3c5df8f
martyna.radz <mar.rad97@gmail.com>
2024-05-04 09:15:11 +0200

    zad2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 import company.beans.Person;
+import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+import company.interfaces.Messenger;
 
 public class Main {
     public static void main(String[] args) {
         try {
             Person person = new Person("Jan Nowak", 30);
             System.out.println("Imie: " + person.getName() + ", Wiek: " + person.getAge());
-        } catch (company.exceptions.InvalidAgeException e) {
+        } catch (InvalidAgeException e) {
             System.err.println(e.getMessage());
         }
-        company.interfaces.Messenger messenger = new company.implementations.EmailMessenger();
+        Messenger messenger = new EmailMessenger();
         messenger.sendMessage("Dzień dobry!");
     }
 }
